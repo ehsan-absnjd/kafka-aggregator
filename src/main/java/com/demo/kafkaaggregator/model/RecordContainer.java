94e4350d5305043ed784de6c4eec04275acb67b9
Ehsan Abbas Nejad <ehsan.abbasnejad@dfreight.org>
2022-12-12 19:11:22 +0330

    optimize RecordContainer

--- src/main/java/com/demo/kafkaaggregator/model/RecordContainer.java
+++ src/main/java/com/demo/kafkaaggregator/model/RecordContainer.java
@@ -12,22 +12,20 @@ import java.util.stream.Collectors;
 public class RecordContainer {
     private final long threshold;
     private Map<String, Long> counts = new HashMap<>();
-    private Map<String, Long> finalResults = new HashMap<>();
 
     public void put(String value) {
         long valueCount = counts.getOrDefault(value, 0L) + 1;
         counts.put(value, valueCount);
-        if (valueCount >= threshold)
-            finalResults.put(value, valueCount);
     }
 
     public Map<String, Long> getResults() {
-        return finalResults;
+        return counts.entrySet().stream().filter(e -> e.getValue() >= threshold)
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
     }
 
     public Set<FilteredRecord> getRecordSet(String producer) {
         Date date = new Date();
-        return finalResults.entrySet().stream()
+        return getResults().entrySet().stream()
                 .map(e -> FilteredRecord.builder().count(e.getValue()).date(date).producer(producer).url(e.getKey()).build())
                 .collect(Collectors.toSet());
     }
